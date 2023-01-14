1f6377b5807e768a010608e6148fbb1b942cde66
IBRAHIM HASSEN <118390866+ibrahimmohammedhassen@users.noreply.github.com>
2023-01-14 14:36:38 +0000

    Initial commit

---
+++ data_files/text-ready.py
@@ -0,0 +1,13 @@
+def search(file_path):
+    print("Searching...")
+    with open(file_path) as file:
+        for line in file.readlines():
+            print(f"Looked in {line.strip()}")
+    print("...Done!")
+
+
+def run():
+    search("data/files/txt/locations.txt")
+
+
+run()
